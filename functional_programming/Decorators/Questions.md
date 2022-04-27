595c08d2a7168d7de3fe26391e637b0a5f870238
Kartik Patil <kartikrp6300@gmail.com>
2022-04-27 17:52:29 +0100

    coding excerices and questions for decorators solved

--- functional_programming/Decorators/Questions.md
+++ functional_programming/Decorators/Questions.md
@@ -1,14 +1,23 @@
 
 What is a decorator?
+a decorator is a function that takes another function and extends the behaviour of the previous function to a the new function.
 
 What is the decorator syntax equivalent to?
+The decorator syntax is equivalent to calling a function inside the pther function to extend the behaviour of the function being called.
 
 What does `functools.wraps` do?
+functools.wraps helps preserve the information about the original function rather than get the identity of the inner function of the decorator.
+
 
 Give some examples of appropriate uses of decorators
+Decorators are useful in debugging code, slowing down functions, registering plugins, caching return values
+
 
 How can decorators be used as a lightweight way to register plugins?
+Decorators can be used to register plug-ins by writing a decorator that has an inner function writing the name of the plug in to a dictionary.
 
 How do you combine multiple decorators on a single function?
+Multiple decorators can be combined by @ them before the start of the function.
 
 How can you make a decorator take arguments?
+A decorator can be made to take arguments using an inner function that takes arguments using *args **kwargs and returning that fucntion
--- functional_programming/Decorators/coding_exercise.py
+++ functional_programming/Decorators/coding_exercise.py
@@ -42,3 +42,66 @@ Hello World!
 filename.txt:
 Hello World!
 """
+import functools
+
+
+#1 Decorator that does stuff before and after function
+def surround(func):
+    @functools.wraps(func)
+    def wrapper():
+        print("the function is about to be run")
+        func()
+        print("the function run is completed")
+    return wrapper
+
+@surround
+def my_func():
+    print("my function is running")
+
+#2 Decorator that takes arguments
+
+def surround1(_func=None, before="before the func", after="after the func"):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper():
+            print("the function is about to be run")
+            func()
+            print("the function run is completed")
+        return wrapper
+    return decorator(_func) if _func else decorator
+
+@surround1(before='The function is about to be run', after='The function run is completed')
+def my_func1():
+    print('Kartik')    
+
+#Decorator that prints args and kwargs that the wrapped funtion is called with
+def log_args_and_kwargs(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f'args={args}', f'kwargs={kwargs}')
+        return func(*args, **kwargs)
+    return wrapper
+
+@log_args_and_kwargs
+def my_func2(*args, **kwargs):
+    pass
+
+def write_to_file(filename):
+    def file_write_decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with open(filename, 'w') as file:
+                file.write(func(*args, **kwargs))
+        return wrapper
+    return file_write_decorator
+
+@write_to_file('Kartik.csv')
+def my_func3():
+    return 'Hello World!'
+
+
+
+my_func()
+my_func1()
+my_func2(0, 1, a=2, b=3)
+my_func3()
